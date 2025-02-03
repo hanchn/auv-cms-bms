054dbad80f20ce91f9fd5cc9dc6d29d9090dddca
yuanjing <jing.yuan@kapeixi.com>
2025-02-03 11:45:48 +0800

    .

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+import router from './router/index.js'
+const app = createApp(App)
+app.use(router).use(Antd).mount('#app')
