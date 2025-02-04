8ebd621332f08855d4afedc1e67fe526c3e9a31d
yuanjing <jing.yuan@kapeixi.com>
2025-02-04 14:26:57 +0800

    .

--- src/views/ColumnList/index.vue
+++ src/views/ColumnList/index.vue
@@ -6,9 +6,15 @@
   </div>
 </template>
 <script setup>
-import { reactive } from 'vue'
+import { reactive, provide } from 'vue'
 import Filter from './components/Filter.vue'
 import Table from './components/Table.vue'
-const state = reactive({})
+const state = reactive({
+  expand: false,
+  formState: {},
+  onFinish: () => { },
+  expandSet: (expand = false) => { state.expand = expand },
+})
+provide('initSate', state)
 </script>
 <style scoped></style>
