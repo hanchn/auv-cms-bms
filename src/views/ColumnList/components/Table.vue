9c1228f4eed93423f864f2c53c225202c84a45aa
yuanjing <jing.yuan@kapeixi.com>
2025-02-04 14:53:42 +0800

    .

--- src/views/ColumnList/components/Table.vue
+++ src/views/ColumnList/components/Table.vue
@@ -2,7 +2,8 @@
   <a-table :columns="state.columns" :data-source="state.dataSource" :scroll="{ x: 1500, y: 300 }">
     <template #bodyCell="{ column }">
       <template v-if="column.key === 'operation'">
-        <a>action</a>
+        <a-button type="link">编辑</a-button>
+        <a-button type="link">创建二级菜单/进入二级菜单</a-button>
       </template>
     </template>
   </a-table>
--- src/views/ColumnList/index.vue
+++ src/views/ColumnList/index.vue
@@ -6,7 +6,7 @@
   </div>
 </template>
 <script setup>
-import { reactive, provide } from 'vue'
+import { reactive, provide, onMounted } from 'vue'
 import Filter from './components/Filter.vue'
 import Table from './components/Table.vue'
 const state = reactive({
@@ -35,34 +35,32 @@ const state = reactive({
   onFinish: () => { },
   expandSet: (expand = false) => { state.expand = expand },
   columns: [
-  { title: 'Full Name', width: 100, dataIndex: 'name', key: 'name', fixed: 'left' },
-  { title: 'Age', width: 100, dataIndex: 'age', key: 'age', fixed: 'left' },
-  { title: 'Column 1', dataIndex: 'address', key: '1', width: 150 },
-  { title: 'Column 2', dataIndex: 'address', key: '2', width: 150 },
-  { title: 'Column 3', dataIndex: 'address', key: '3', width: 150 },
-  { title: 'Column 4', dataIndex: 'address', key: '4', width: 150 },
-  { title: 'Column 5', dataIndex: 'address', key: '5', width: 150 },
-  { title: 'Column 6', dataIndex: 'address', key: '6', width: 150 },
-  { title: 'Column 7', dataIndex: 'address', key: '7', width: 150 },
-  { title: 'Column 8', dataIndex: 'address', key: '8' },
+  { title: 'ID', width: 100, dataIndex: 'id', key: 'id', fixed: 'left' },
+  { title: '标题', width: 100, dataIndex: 'title', key: 'title', fixed: 'left' },
+  { title: '描述', dataIndex: 'desc', key: '1', width: 150 },
+  { title: '启用状态', dataIndex: 'status', key: '2', width: 150 },
+  { title: '创建日期', dataIndex: 'date', key: '3', width: 150 },
   {
-    title: 'Action',
+    title: '操作',
     key: 'operation',
     fixed: 'right',
     width: 100,
   },],
   dataSource: []
 })
-let data = []
-for (let i = 0; i < 100; i++) {
-  data.push({
-    key: i,
-    name: `Edrward ${i}`,
-    age: 32,
-    address: `London Park no. ${i}`,
-  });
-}
-state.dataSource = state.data
+onMounted(() => {
+  let data = []
+  for (let i = 0; i < 100; i++) {
+    data.push({
+      key: i,
+      name: `Edrward ${i}`,
+      age: 32,
+      address: `London Park no. ${i}`,
+    });
+  }
+  state.dataSource = data
+  console.log(state.dataSource) 
+})
 provide('initSate', state)
 </script>
 <style scoped></style>
