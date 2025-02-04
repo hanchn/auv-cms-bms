67200b17dcb6ea33a55a866681e001d6f66cd50e
yuanjing <jing.yuan@kapeixi.com>
2025-02-04 13:59:59 +0800

    .

--- src/views/ColumnList/index.vue
+++ src/views/ColumnList/index.vue
@@ -1,7 +1,14 @@
 <!--栏目列表-->
-<template>Articles</template>
+<template>
+  <div>
+    <Filter/>
+    <Table/>
+  </div>
+</template>
 <script setup>
 import { reactive } from 'vue'
+import Filter from './components/Filter.vue'
+import Table from './components/Table.vue'
 const state = reactive({})
 </script>
 <style scoped></style>
